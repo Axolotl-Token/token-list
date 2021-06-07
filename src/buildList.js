b36fdcf946bc1317f8ac191ac7757a4d5057c3ff
Axototl Finance <84419140+Axolotl-Token@users.noreply.github.com>
2021-06-07 12:59:07 -0500

    add bsc

--- src/buildList.js
+++ src/buildList.js
@@ -4,6 +4,7 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");
+const bsc = require("./tokens/bsc.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -18,7 +19,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmRYpYpviWe2wb4D4u3rcawA1v1tvG7jqaFF4QfDDV8HjP",
     keywords: ["axolotl", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...bsc]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
